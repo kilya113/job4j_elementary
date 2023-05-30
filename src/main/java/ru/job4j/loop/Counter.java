a9bd084ad56ea805a17684e90c8e15cf2795a440
kilya113 <job4jstart@gmail.com>
2023-05-31 01:49:36 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192  [#192 #198943]]

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+}
