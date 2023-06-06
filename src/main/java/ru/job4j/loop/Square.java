7d6f723d97c3b79acab2f4f7ae11247f45502287
kilya113 <job4jstart@gmail.com>
2023-06-06 22:37:54 +0300

    6.0.7. Массивы в тестах. [#226789  [#226789 #201975]]

--- src/main/java/ru/job4j/loop/Square.java
+++ src/main/java/ru/job4j/loop/Square.java
@@ -6,7 +6,6 @@ public class Square {
         for (int i = 0; i < bound; i++) {
             rst[i] = (int) Math.pow(i, 2);
         }
-        /* заполнить массив через цикл элементами от 0 до bound , возведенными в квадрат */
         return rst;
     }
 
